a5447a78cf98ba5f62ade91a0df0509882bc6e04
Noah <jigutierrez@estudiantec.cr>
2026-05-06 21:25:17 -0600

    cambio counter

--- Proyecto2/src/design/counter.sv
+++ Proyecto2/src/design/counter.sv
@@ -1,23 +1,20 @@
 module counter (
-    input wire clk,          // 27 MHz
-    output reg [1:0] sel     // selección de dígito
+    input wire clk,
+    output reg [1:0] sel = 0
 );
 
     reg [14:0] clk_div = 0;
-    reg clk_scan = 0;
 
-    // divisor de reloj (~1 kHz)
     always @(posedge clk) begin
-        clk_div <= clk_div + 1;
+
         if (clk_div == 27000) begin
             clk_div <= 0;
-            clk_scan <= ~clk_scan;
+            sel <= sel + 1;
         end
-    end
+        else begin
+            clk_div <= clk_div + 1;
+        end
 
-    // contador MOD-4
-    always @(posedge clk_scan) begin
-        sel <= sel + 1;
     end
 
 endmodule
