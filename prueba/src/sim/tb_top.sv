d79ca2675fd77e470df3b962574217a94052d3cd
Noah <jigutierrez@estudiantec.cr>
2026-05-12 17:13:36 -0600

    edit 6

--- prueba/src/sim/tb_top.sv
+++ prueba/src/sim/tb_top.sv
@@ -167,7 +167,7 @@ module tb_top;
     // ============================================================
     initial begin
 
-        $dumpfile("tb_top.vcd");
+        $dumpfile("sim/tb_top.vcd");
         $dumpvars(0, tb_top);
 
         filas_raw = 0;
